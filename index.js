a8fe0666a7d0d470f28346e6c8e2452444a11e7a
michaeldavidpryor <mdpcaps@gmail.com>
2020-05-09 01:50:05 -0400

    added layout for index.js and generatemarkdown files

--- index.js
+++ index.js
@@ -6,77 +6,27 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 
 inquirer
-  .prompt({
-    message: "Enter your GitHub username:",
-    name: "username"
-  })
-function generateMD(answers) {
-  return `
-# Project Title
+  .prompt([
+    {
+      type: "input",
+      message: "Enter your GitHub username:",
+      name: "username"
+    },
 
-## Description
+    {
+      type: "input",
+      message: "Enter your GitHub username:",
+      name: "username"
+    },
+  ]);
+};
 
-## Installation
+function writeToFile(fileName, data) {
 
-## Table of Contents
-
-- [Installation](#installation)
-- [Features](#features)
-- [Contributing](#contributing)
-- [Team](#team)
-- [FAQ](#faq)
-- [Support](#support)
-- [License](#license)
-
-
-## Usage
-
-```
-    ```
-## Installation
-
-
-## Tests
-- Going into more detail on code and technologies used
-
-## Contributing
-Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
-
-Please make sure to update tests as appropriate.
-
-## License
-[MIT](https://choosealicense.com/licenses/mit/)
-
-![GitHub followers](https://img.shields.io/github/followers/${username}?label=Follow&style=social);
- }`
-
-    .then(function ({ username }) {
-      const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
-
-      axios.get(queryUrl).then(function (res) {
-        const repoNames = res.data.map(function (repo) {
-          return repo.name;
-        });
-
-        const repoNamesStr = repoNames.join("\n");
-
-        fs.writeFile("repos.txt", repoNamesStr, function (err) {
-          if (err) {
-            throw err;
-          }
-
-          console.log(`Saved ${repoNames.length} repos`);
-        });
-      });
-    });
-
-  function writeToFile(fileName, data) {
-
-  }
-
-  function init() {
-
-  }
-
-  init();
 }
+
+function init() {
+
+}
+
+init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,41 @@
-function generateMarkdown(data) {
+function generateMarkdown(data, url) {
+  console.log(data)
   return `
 # ${data.title}
 
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Tests](#tests)
+- [Contributing](#contributing)
+- [License](#license)
+- [Contact](#contact)
+
+
+## Description
+
+## Installation
+
+## Usage
+
+```
+    ```
+
+## Tests
+- Going into more detail on code and technologies used
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+(https://choosealicense.com/licenses)
+
+![GitHub followers](https://img.shields.io/github/followers/${username}?label=Follow&style=social);
+ }
 `;
 }
 
